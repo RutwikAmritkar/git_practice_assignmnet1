0631a184377db60a0ab381202b51af0cf873fb04
RutwikAmritkar01 <rutwikamritkar007@gmail.com>
2023-04-29 15:19:26 +0530

    prime.js

---
+++ prime.js
@@ -0,0 +1,13 @@
+function checkPrime(num){
+factor=0;
+ for(let Si=0;i<num;i++){
+    if(num%i==0){
+       factor++;
+       }
+    }
+  if(factor==2){
+     return true;
+     }
+     else{
+        false;
+     }
